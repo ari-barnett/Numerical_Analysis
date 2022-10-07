6db784e2570d02add082149f05cf6b260174b4fb
ari-barnett <72417784+ari-barnett@users.noreply.github.com>
2022-10-07 19:53:09 -0400

    Update NewtonsMethod.py

--- NewtonsMethod.py
+++ NewtonsMethod.py
@@ -3,18 +3,16 @@ from sympy import *
 x = symbols('x')
 
 def func():
-    #return x**2 + x - 2;
-    #return(x**8 - 36*x**7 + 546*x**6 - 4536*x**5 + 22449*x**4 - 67284*x**3 + 118124*x**2 - 109584*x + 40320)
-    return (tan(x)-x)
-    #return (x**-1 - 2)
+    return (tan(x)-x) #Symbolic definition of the function Tan(x) = x
+
 
 def NewtonsMethod(f,x0,M,TOL):
     i = 1;
-    while abs(f.subs(x,x0)) > TOL and i<M:
+    while abs(f.subs(x,x0)) > TOL and i<M: 
         
-        y0 = f.subs(x,x0);
-        yp = diff(f,x)
-        x1 = x0 - y0/yp.subs(x,x0);
+        y0 = f.subs(x,x0); #Function evaluation at xn
+        yp = diff(f,x) #Derivative of the funcation
+        x1 = x0 - y0/yp.subs(x,x0); # Newtons algorithm for xn1
         
         x0 = x1;
         i = i+1;
